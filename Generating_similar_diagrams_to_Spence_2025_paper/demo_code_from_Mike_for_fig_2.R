8a2315a10215d7c32ee7df6386fe83fc16a22acb
Emma Bowen <dtj504@york.ac.uk>
2026-02-03 16:43:08 +0000

    Fixing first set of files due ot removal of prisk 1

--- Generating_similar_diagrams_to_Spence_2025_paper/demo_code_from_Mike_for_fig_2.R
+++ Generating_similar_diagrams_to_Spence_2025_paper/demo_code_from_Mike_for_fig_2.R
@@ -99,36 +99,66 @@ exclude3 <-dat_all2$x[which(qs3[,4]<(best3 - 1e-8))]
 
 # PLOTTING
 
+# load libraries
 library(grDevices)
+# give file a name and dimensions
 cairo_ps("toy_1.eps",width=5.5,height=5)
+# create a grid of four plots
 par(mfrow=c(2,2))
+# set out outer margins
 par(oma=c(2,2,1,1))
+# set margins for this plot
 par(mar=c(3,3,0,0))
+
+# set point size to be 1.3x default
 cex_arg <- 1.3
+
+# COULD MAKE THE Y AXIS THE SAME AS IN LATER PLOTS FOR FIRST PLOT FOR CONSISTENCY
+# AND THSU EASIER VISUAL COMPARISON
+
+# first plot - just data points
 plot(dat1$x,dat1$y,xlim=c(0,2),ylim=c(y_fun(2),y_fun(0.75)),pch=16,xaxs="i",xlab="",ylab="",cex=cex_arg) ## the output of the
+# labels the plot "a)" for later reference
 text(0.1,y_fun(0.75),labels ="a)")
+
+# second plot - data points and simulated functions
 plot(dat1$x,dat1$y,xlim=c(0,2),ylim=c(y_fun(2),max(qs1)),xaxs="i",pch=16,xlab="",ylab="",cex=cex_arg)
+# adding layers for the simulated functions
 for(i in 1:nsim){
 lines(dat_all$x,sim_val[i,])
 }
+# again, adding a label "b)" for later reference
 text(0.1,max(qs1),labels ="b)")
 
+# third plot - data points, uncertainty bands, 99.99% bound, excluded points
 plot(dat1$x,dat1$y,xlim=c(0,2),ylim=c(y_fun(2),max(qs1)),xaxs="i",pch=16,xlab="",ylab="",cex=cex_arg)
 SpenceTools::uncertain_plot(dat_all$x,t(qs1[,1:3]),add=T,lwd = 1,lty=2)
 #lines(dat_all$x,qs1[,1],lty=3)
 lines(dat_all$x,qs1[,4],lty=3)
+# adds line at the best y value so far
 abline(h=best_so_far)
+# places the x marks along the bottom on all invalid points
 points(exclude,rep(0,length(exclude)),pch=4)
+# adds label "c)" for later reference
 text(0.1,max(qs1),labels ="c)")
 
+# fourth plot - data points, uncertainty bands, 99.99% bound, excluded points for round 2
 plot(dat2$x,dat2$y,xlim=c(0,2),ylim=c(y_fun(2),max(qs1)),xaxs="i",pch=16,xlab="",ylab="",cex=cex_arg)
 SpenceTools::uncertain_plot(dat_all2$x,t(qs2[,1:3]),add=T,lwd = 1,lty=2)
 #lines(dat_all$x,qs1[,1],lty=3)
 lines(dat_all$x,qs2[,4],lty=3)
+# line adding best point found os far after roudn 2
 abline(h=best2)
+# mark points we have excluded with an x along the bottom
 points(exclude2,rep(0,length(exclude2)),pch=4)
+# adds label "d)" for reference
 text(0.1,max(qs1),labels ="d)")
 
+
+
+##THESE WERE COMMENTED OUT BUT ANALYSING FOR NOW
+
+# # plot for third round of sampling
 # plot(dat3$x,dat3$y,xlim=c(0,2),ylim=c(y_fun(2),max(qs1)),xaxs="i",pch=16,cex=cex_arg)
 # SpenceTools::uncertain_plot(dat_all3$x,t(qs3[,1:3]),add=T,lwd = 1,lty=2)
 # #lines(dat_all$x,qs1[,1],lty=3)
@@ -136,7 +166,8 @@ text(0.1,max(qs1),labels ="d)")
 # abline(h=best3)
 # points(exclude3,rep(0,length(exclude3)),pch=4)
 # text(0.1,max(qs1),labels ="e)")
-# 
+
+# # zoomed in plot for third round of sampling
 # plot(dat3$x,dat3$y,xlim=c(0.6,0.9),ylim=c(y_fun(0.9),max(qs3)),xaxs="i",pch=16,cex=cex_arg)
 # SpenceTools::uncertain_plot(dat_all3$x,t(qs3[,1:3]),add=T,lwd = 1,lty=2)
 # #lines(dat_all$x,qs1[,1],lty=3)
@@ -146,8 +177,15 @@ text(0.1,max(qs1),labels ="d)")
 # axis(1,at = seq(0.6,0.9,0.01),labels=rep("",length(seq(0.6,0.9,0.01))))
 # text(0.62,max(qs3),labels ="f)")
 
+
+
+
+
+# add x axis label for all plots of theta
 mtext(expression(theta),1,outer=T,line=0)
+# add y axis label for all plots of f(theta)
 mtext(expression(paste(f(theta))),2,outer=T,line=0)
+# save and close file
 dev.off()
 
 
@@ -156,55 +194,58 @@ dev.off()
 #### comparing GP kernels - THIS IS SUPP MATERIAL FIG S1 (already done)
 
 # could run this just to get dashed lien instead and to double check
-num_round <- 5
-xs <- SpenceTools::specific_floor(seq(0.2,1.8,length.out=num_round),digits=2)
-dat1 <- data.frame(x=xs,y=y_fun(xs))
-gpd_1 <- km(~1,design=as.matrix(dat1$x),estim.method="MLE",response = dat1$y,covtype = "gauss",nugget=1e-8*var(dat1$y))
-gpd_2 <- km(~1,design=as.matrix(dat1$x),estim.method="MLE",response = dat1$y,covtype = "matern5_2",nugget=1e-8*var(dat1$y))
-gpd_3 <- km(~1,design=as.matrix(dat1$x),estim.method="MLE",response = dat1$y,covtype = "matern3_2",nugget=1e-8*var(dat1$y))
-gpd_4 <- km(~1,design=as.matrix(dat1$x),estim.method="MLE",response = dat1$y,covtype = "powexp",nugget=1e-8*var(dat1$y))
-gpd_5 <- km(~1,design=as.matrix(dat1$x),estim.method="MLE",response = dat1$y,covtype = "exp",nugget=1e-8*var(dat1$y))
 
 
 
-predd_1_gp <- predict(gpd_1,newdata=as.matrix(dat_all$x),type="SK")
-datd_all$y1 <- predd_1_gp$mean
-qsd1 <- as.numeric(datd_all$y1) +cbind(qnorm(0.05,0,predd_1_gp$sd),0,qnorm(0.95,0,predd_1_gp$sd),qnorm(cert,0,predd_1_gp$sd))
-###
-predd_2_gp <- predict(gpd_2,newdata=as.matrix(dat_all$x),type="SK")
-datd_all$y2 <- predd_2_gp$mean
-qsd2 <- as.numeric(datd_all$y2) +cbind(qnorm(0.05,0,predd_2_gp$sd),0,qnorm(0.95,0,predd_2_gp$sd),qnorm(cert,0,predd_2_gp$sd))
-###
-predd_3_gp <- predict(gpd_3,newdata=as.matrix(dat_all$x),type="SK")
-datd_all$y3 <- predd_3_gp$mean
-qsd3 <- as.numeric(datd_all$y3) +cbind(qnorm(0.05,0,predd_3_gp$sd),0,qnorm(0.95,0,predd_3_gp$sd),qnorm(cert,0,predd_3_gp$sd))
-###
-predd_4_gp <- predict(gpd_4,newdata=as.matrix(dat_all$x),type="SK")
-datd_all$y4 <- predd_4_gp$mean
-qsd4 <- as.numeric(datd_all$y4) +cbind(qnorm(0.05,0,predd_4_gp$sd),0,qnorm(0.95,0,predd_4_gp$sd),qnorm(cert,0,predd_4_gp$sd))
-###
-predd_5_gp <- predict(gpd_5,newdata=as.matrix(dat_all$x),type="SK")
-datd_all$y5 <- predd_5_gp$mean
-qsd5 <- as.numeric(datd_all$y5) +cbind(qnorm(0.05,0,predd_5_gp$sd),0,qnorm(0.95,0,predd_5_gp$sd),qnorm(cert,0,predd_5_gp$sd))
+# num_round <- 5
+# xs <- SpenceTools::specific_floor(seq(0.2,1.8,length.out=num_round),digits=2)
+# dat1 <- data.frame(x=xs,y=y_fun(xs))
+# gpd_1 <- km(~1,design=as.matrix(dat1$x),estim.method="MLE",response = dat1$y,covtype = "gauss",nugget=1e-8*var(dat1$y))
+# gpd_2 <- km(~1,design=as.matrix(dat1$x),estim.method="MLE",response = dat1$y,covtype = "matern5_2",nugget=1e-8*var(dat1$y))
+# gpd_3 <- km(~1,design=as.matrix(dat1$x),estim.method="MLE",response = dat1$y,covtype = "matern3_2",nugget=1e-8*var(dat1$y))
+# gpd_4 <- km(~1,design=as.matrix(dat1$x),estim.method="MLE",response = dat1$y,covtype = "powexp",nugget=1e-8*var(dat1$y))
+# gpd_5 <- km(~1,design=as.matrix(dat1$x),estim.method="MLE",response = dat1$y,covtype = "exp",nugget=1e-8*var(dat1$y))
 
-cairo_ps("diff_kernel.eps",width=5.5,height=5)
-par(mfrow=c(3,2))
-par(oma=c(2,2,1,1))
-par(mar=c(3,3,2,0))
-plot(dat1$x,dat1$y,xlim=c(0,2),ylim=c(y_fun(2),max(qs1)),xaxs="i",pch=16,main="gauss",xlab="",ylab="")
-SpenceTools::uncertain_plot(dat_all$x,t(qsd1[,1:3]),add=T,lwd = 1,lty=2)
-#
-plot(dat1$x,dat1$y,xlim=c(0,2),ylim=c(y_fun(2),max(qs1)),xaxs="i",pch=16,main="Matern5_2",xlab="",ylab="")
-SpenceTools::uncertain_plot(dat_all$x,t(qsd2[,1:3]),add=T,lwd = 1,lty=2)
-#
-plot(dat1$x,dat1$y,xlim=c(0,2),ylim=c(y_fun(2),max(qs1)),xaxs="i",pch=16,main="Matern3_2",xlab="",ylab="")
-SpenceTools::uncertain_plot(dat_all$x,t(qsd3[,1:3]),add=T,lwd = 1,lty=2)
-#
-plot(dat1$x,dat1$y,xlim=c(0,2),ylim=c(y_fun(2),max(qs1)),xaxs="i",pch=16,main="powexp",xlab="",ylab="")
-SpenceTools::uncertain_plot(dat_all$x,t(qsd4[,1:3]),add=T,lwd = 1,lty=2)
-#
-plot(dat1$x,dat1$y,xlim=c(0,2),ylim=c(y_fun(2),max(qs1)),xaxs="i",pch=16,main="exp",xlab="",ylab="")
-SpenceTools::uncertain_plot(dat_all$x,t(qsd5[,1:3]),add=T,lwd = 1,lty=2)
-mtext(expression(theta),1,outer=T,line=0)
-mtext(expression(paste(f(theta))),2,outer=T,line=0)
-dev.off()
+
+
+# predd_1_gp <- predict(gpd_1,newdata=as.matrix(dat_all$x),type="SK")
+# datd_all$y1 <- predd_1_gp$mean
+# qsd1 <- as.numeric(datd_all$y1) +cbind(qnorm(0.05,0,predd_1_gp$sd),0,qnorm(0.95,0,predd_1_gp$sd),qnorm(cert,0,predd_1_gp$sd))
+# ###
+# predd_2_gp <- predict(gpd_2,newdata=as.matrix(dat_all$x),type="SK")
+# datd_all$y2 <- predd_2_gp$mean
+# qsd2 <- as.numeric(datd_all$y2) +cbind(qnorm(0.05,0,predd_2_gp$sd),0,qnorm(0.95,0,predd_2_gp$sd),qnorm(cert,0,predd_2_gp$sd))
+# ###
+# predd_3_gp <- predict(gpd_3,newdata=as.matrix(dat_all$x),type="SK")
+# datd_all$y3 <- predd_3_gp$mean
+# qsd3 <- as.numeric(datd_all$y3) +cbind(qnorm(0.05,0,predd_3_gp$sd),0,qnorm(0.95,0,predd_3_gp$sd),qnorm(cert,0,predd_3_gp$sd))
+# ###
+# predd_4_gp <- predict(gpd_4,newdata=as.matrix(dat_all$x),type="SK")
+# datd_all$y4 <- predd_4_gp$mean
+# qsd4 <- as.numeric(datd_all$y4) +cbind(qnorm(0.05,0,predd_4_gp$sd),0,qnorm(0.95,0,predd_4_gp$sd),qnorm(cert,0,predd_4_gp$sd))
+# ###
+# predd_5_gp <- predict(gpd_5,newdata=as.matrix(dat_all$x),type="SK")
+# datd_all$y5 <- predd_5_gp$mean
+# qsd5 <- as.numeric(datd_all$y5) +cbind(qnorm(0.05,0,predd_5_gp$sd),0,qnorm(0.95,0,predd_5_gp$sd),qnorm(cert,0,predd_5_gp$sd))
+
+# cairo_ps("diff_kernel.eps",width=5.5,height=5)
+# par(mfrow=c(3,2))
+# par(oma=c(2,2,1,1))
+# par(mar=c(3,3,2,0))
+# plot(dat1$x,dat1$y,xlim=c(0,2),ylim=c(y_fun(2),max(qs1)),xaxs="i",pch=16,main="gauss",xlab="",ylab="")
+# SpenceTools::uncertain_plot(dat_all$x,t(qsd1[,1:3]),add=T,lwd = 1,lty=2)
+# #
+# plot(dat1$x,dat1$y,xlim=c(0,2),ylim=c(y_fun(2),max(qs1)),xaxs="i",pch=16,main="Matern5_2",xlab="",ylab="")
+# SpenceTools::uncertain_plot(dat_all$x,t(qsd2[,1:3]),add=T,lwd = 1,lty=2)
+# #
+# plot(dat1$x,dat1$y,xlim=c(0,2),ylim=c(y_fun(2),max(qs1)),xaxs="i",pch=16,main="Matern3_2",xlab="",ylab="")
+# SpenceTools::uncertain_plot(dat_all$x,t(qsd3[,1:3]),add=T,lwd = 1,lty=2)
+# #
+# plot(dat1$x,dat1$y,xlim=c(0,2),ylim=c(y_fun(2),max(qs1)),xaxs="i",pch=16,main="powexp",xlab="",ylab="")
+# SpenceTools::uncertain_plot(dat_all$x,t(qsd4[,1:3]),add=T,lwd = 1,lty=2)
+# #
+# plot(dat1$x,dat1$y,xlim=c(0,2),ylim=c(y_fun(2),max(qs1)),xaxs="i",pch=16,main="exp",xlab="",ylab="")
+# SpenceTools::uncertain_plot(dat_all$x,t(qsd5[,1:3]),add=T,lwd = 1,lty=2)
+# mtext(expression(theta),1,outer=T,line=0)
+# mtext(expression(paste(f(theta))),2,outer=T,line=0)
+# dev.off()
